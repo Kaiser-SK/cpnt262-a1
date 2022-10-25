6ffcb54b501ea9defa81c9dfbdbd859e113bb1b8
Sebastian Kaiser <sebastian.kaiser@edu.sait.ca>
2022-10-24 22:48:18 -0600

    coding

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# cpnt262-a1
+# Web Developer Fast Track - CPNT 262
+
+## Assignment 1 - Push of a Button
+
+## Sebastian Kaiser
+
+### GH Pages
+
+
+#### Attributions
+
+<https://unsplash.com/photos/Mv9hjnEUHR4>
+<https://unsplash.com/photos/Y0WXj3xqJz0>
+<https://fontawesome.com/icons/cat?s=solid&f=classic>
+<https://fontawesome.com/icons/dog?s=solid&f=classic>
---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const showCutePug =
+  document.querySelector('.dog-button').onclick = function() {
+  document.querySelector('.pug-image').style.display= "block";
+  document.querySelector('.animal-prompt').innerHTML = "Dogs are the best!"
+}
+
+const showCuteKitten =
+  document.querySelector('.cat-button').onclick = function() {
+  document.querySelector('.kitten-image').style.display= "block";
+  document.querySelector('.animal-prompt').innerHTML = "Cats are the best!"
+}
